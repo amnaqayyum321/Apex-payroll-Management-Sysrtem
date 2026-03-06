d52db8a5b6284b2ce43bd00e36e5036df3292712
arifjamal317 <arifjamalchohan317@gmail.com>
2026-03-06 14:15:30 +0500

    Onboarding Minor Chnages

--- src/app/features/forms/master-data/view-work-schedule/view-work-schedule.ts
+++ src/app/features/forms/master-data/view-work-schedule/view-work-schedule.ts
@@ -9,13 +9,12 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-view-work-schedule',
-  imports: [CommonModule,FormsModule, RouterModule, NgbDropdownModule],
+  imports: [CommonModule, FormsModule, RouterModule, NgbDropdownModule],
   templateUrl: './view-work-schedule.html',
   styleUrl: './view-work-schedule.scss',
 })
 export class ViewWorkSchedule {
-
-   DesignationsList: any[] = [];
+  WorkScheduleList: any[] = [];
   totalItems: number = 0;
   totalPagesCount: number = 0;
 
@@ -27,10 +26,9 @@ export class ViewWorkSchedule {
 
   currentPage = 1;
   itemsPerPage = 7;
-  paginatedDesigationList: any[] = [];
+  paginatedWorkScheduleList: any[] = [];
   publicId: string | null = null;
   isEditMode = false;
-
 
   get totalPages() {
     return this.totalPagesCount || Math.ceil(this.totalItems / this.itemsPerPage);
@@ -47,15 +45,15 @@ export class ViewWorkSchedule {
   loadDesigantions() {
     this.loader.show();
     const backendPage = this.currentPage - 1;
-    this.formsService.getAllWorkSchedules(backendPage, this.itemsPerPage,  'ALL').subscribe({
+    this.formsService.getAllWorkSchedules(backendPage, this.itemsPerPage, 'ALL').subscribe({
       next: (response: any) => {
         this.loader.hide();
-        this.DesignationsList = response.data;
-        console.log(this.DesignationsList);
+        this.WorkScheduleList = response.data;
+        console.log(this.WorkScheduleList);
         this.totalItems = response.paginator.totalItems;
         this.totalPagesCount = response.paginator.totalPages;
         this.currentPage = response.paginator.currentPage + 1; // Backend 0-indexed
-        this.paginatedDesigationList = this.DesignationsList;
+        this.paginatedWorkScheduleList = this.WorkScheduleList;
       },
       error: (error) => {
         this.loader.hide();
@@ -78,5 +76,4 @@ export class ViewWorkSchedule {
       .replace(/_/g, ' ')
       .replace(/\b\w/g, (c) => c.toUpperCase());
   }
-
 }
