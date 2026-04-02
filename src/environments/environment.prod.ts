717555dc833a899a0b8e4a35ac732bfd1ef24e9b
arifjamal317 <arifjamalchohan317@gmail.com>
2026-04-02 16:17:23 +0500

    Attachment done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://213.199.62.174:3300/api/', // dev backend
+  // apiBaseUrl: 'http://213.199.62.174:3300/api/', // dev backend
+  apiBaseUrl: 'http://207.180.196.188:3300/api/', // dev backend
   enableDebug: true,
 };
